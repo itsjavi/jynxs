53dcf6da966d8a90c5e219d45150cc3f2cf51b3a
Javi Aguilar <122741+itsjavi@users.noreply.github.com>
2024-09-05 08:27:32 +0200

    improve landing page

--- src/example.tsx
+++ src/example.tsx
@@ -3,6 +3,7 @@ import './example.css'
 import { cleanup, renderJSX, useEffect, useState } from './runtime/jsx-runtime'
 import libLogo from '/icon.png'
 import viteLogo from '/vite.svg'
+
 // Async component
 const AsyncComponent = async () => {
   await new Promise((resolve) => setTimeout(resolve, 2000)) // Simulate delay
@@ -28,30 +29,56 @@ const App = () => {
 
   const handleClick = () => {
     if (inputRef.current) {
-      alert(`Input value: ${inputRef.current.value}`)
+      alert(`inputRef.current.value = ${inputRef.current.value}`)
     }
   }
 
   return (
     <div>
-      <a href="https://vitejs.dev" target="_blank">
+      <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
         <img src={viteLogo} class="logo" alt="Vite logo" />
       </a>
-      <a href="https://github.com/itsjavi/jynxs" target="_blank">
-        <img src={libLogo} class="logo vanilla" alt="TypeScript logo" />
+      <a href="https://github.com/itsjavi/jynxs" target="_blank" rel="noreferrer">
+        <img src={libLogo} class="logo jynxs-logo" alt="TypeScript logo" />
       </a>
-      <h1 key={123}>Vite + JynXS</h1>
-      <input ref={inputRef} placeholder="Type something..." />
-      <button onClick={handleClick}>Alert Input Value</button>
-      <p>Count: {count}</p>
-      <button onClick={() => setCount(count + 1)}>Increment Count</button>
+      <h1 key={123}>
+        Vite +{' '}
+        <a href="https://github.com/itsjavi/jynxs" target="_blank" rel="noreferrer">
+          JynXS
+        </a>
+      </h1>
+      <p class="description">
+        JynXS is a lightweight, JSX runtime that implements the very basics of a modern UI library without relying on
+        React.
+        <br />
+        <br />
+        Examples:
+      </p>
+      <div class="flex-y">
+        <div class="flex-x">
+          <input ref={inputRef} placeholder="Type something..." />
+          <button type="button" onClick={handleClick}>
+            Alert Input Value
+          </button>
+        </div>
+        <div class="flex-x">
+          <button type="button" onClick={() => setCount(count + 1)}>
+            Increment Count
+          </button>
+          <div>Count: {count}</div>
+        </div>
+      </div>
       <AsyncComponent fallback={<p>Loading async component...</p>} />
     </div>
   )
 }
 
 // Initial render
-renderJSX(<App />, document.getElementById('app')!)
+const appElement = document.getElementById('app')
+if (!appElement) {
+  throw new Error('No element with id "app" found')
+}
+renderJSX(<App />, appElement)
 
 // Cleanup on window unload
 window.addEventListener('beforeunload', cleanup)
